8813f423ca8ef623e44f782aa1008dc161be39ea
Sachin8933 <sachintkr632@gmail.com>
2023-01-02 01:38:08 +0530

    first commit

---
+++ js/questions.js
@@ -0,0 +1,116 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+  {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+  {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+  {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+  {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  {
+    numb: 6,
+    question: "What is software?",
+    answer: "Instructions that tell the hardware what to do",
+    options: [
+      "Clothing designed to be worn by computer users",
+      "Instructions that tell the hardware what to do",
+      "Flexible parts of a computer case",
+      "Any part of the computer that has a physical structure"
+    ]
+  },
+  {
+    numb: 7,
+    question: "The computer’s main circuit board is called a_______",
+    answer: "motherboard",
+    options: [
+      "hard drive",
+      "Bluetooth card",
+      "monitor",
+      "motherboard"
+    ]
+  },
+  {
+    numb: 8,
+    question: "What is an Ethernet port used for?",
+    answer: "Connecting to the Internet",
+    options: [
+      "Connecting smartphones and other peripherals",
+      "Providing power to the computer",
+      "Connecting to the Internet",
+      "Creating new user accounts"
+    ]
+  },
+  {
+    numb: 9,
+    question: "What is Wi-Fi?",
+    answer: "A type of wireless network",
+    options: [
+      "A type of wireless network",
+      "A type of sound card",
+      "An extra-wide computer case used by servers",
+      "A type of software that scans for viruses"
+    ]
+  },
+
+  {
+    numb: 10,
+    question: "What does 'GUI' stand for?",
+    answer: "Graphical user interface",
+    options: [
+      "Good used iPhone",
+      "Graphical user interface",
+      "Gateway using Intel",
+      "Global user index"
+    ]
+  },
+
+
+];
